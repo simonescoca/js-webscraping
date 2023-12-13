b02af9d7b30c296d9c497ab8ef0e2840ef250d05
simonescoca <simone.scoca@icloud.com>
2023-12-13 19:47:23 +0100

    added delay for scrape func to cycle urls array

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-const puppeteer = require('puppeteer');
+const scrape = require("./scrape-function");
 const fs = require('fs');
+const brands = require("./brands");
+const { url } = require("inspector");
 
-// const indirizzo = 'https://www.automoto.it/catalogo/mini/mini/20-john-cooper-works/157770';
-const indirizzo = 'https://www.automoto.it/catalogo/audi/r8-coupe/52-fsi-quattro-s-tronic-plus/123864';
+const urls = [];
 
-scrape(indirizzo)
+brands.forEach((brand) => {
+    brand.models.forEach((model) => {
+        urls.push(model.url);
+    });
+});
+
+const urlsFinalIndex = urls.length - 1;
+let index = 0;
+
+const wait = setInterval(() => {
+
+    scrape(urls[index])
     .then((data) => {
         let jsonData = [];
-
+    
         try {
-            console.log('> leggo il file cars-specs.json...')
+            console.log('> leggo il file cars-specs.json...');
             const fileData = fs.readFileSync('cars-specs.json', 'utf8');
             jsonData = JSON.parse(fileData);
         } catch (err) {
@@ -20,35 +32,18 @@ scrape(indirizzo)
                 fs.writeFileSync('cars-specs.json', '[]', { flag: 'w' });
             }
         }
-
-        console.log('> aggiungo il nuovo oggetto con i dati al file cars-specs.json...')
+    
+        console.log('> aggiungo il nuovo oggetto con i dati al file cars-specs.json...');
         jsonData.push(data);
         fs.writeFileSync('cars-specs.json', JSON.stringify(jsonData, null, 2), { flag: 'w' });
+    })
+    .then(() => {
+        if(index === urlsFinalIndex) clearInterval(wait);
+        index++;
     })
     .catch((err) => {
         console.log('Errore: ' + err.message);
     });
 
-async function scrape (url) {
+}, 30000);
 
-    const browser = await puppeteer.launch({headless: 'new'});
-    const page = await browser.newPage();
-    await page.goto(url);
-
-    const tableRows = await page.$$('.datagrid > tbody > tr');
-    const data = {};
-
-    for(const tableRow of tableRows) {
-        const th = await page.evaluate(element => element.querySelector('th').textContent, tableRow);
-        const td = await page.evaluate(element => element.querySelector('td').textContent, tableRow);
-
-        const key = th.replace(/\s{2,}/g, ' ').trim();
-        const value = td.replace(/\s{2,}/g, ' ').trim();
-
-        // if(key.toLowerCase() === 'consumo combinato') break;
-        data[key] = value;
-    }
-
-    await browser.close();
-    return data;
-};
