042d1a22594d62b012b4f38dd4600aae383d0e4a
simonescoca <simone.scoca@icloud.com>
2023-12-14 19:47:00 +0100

    moved functions

---
+++ src/utils/functions/ricorsiva-function.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const path = require("path");
+const scrape = require("./scrape-function"); // importo la mia funzione di scrape che si serve di puppeteer
+
+/**
+ * La funzione ricorsiva() fa partire la funzione scrape() una volta ogni tot, con tot casuale compreso tra 8 e 16 secondi,
+ * per evitare ban da parte del server del sito.
+ * Inoltre la ricorsiva() si serve del modulo fs per leggere (se esiste) il cars-specs.json e vedere quali auto sono già state scaricate
+ * per evitare di ripetere lo scrape.
+ * Se il file cars-specs.json non dovesse esistere lo creerà e ci inserirà, sotto forma di oggetti, tutte quelle auto rappresentate da un URL in brands.js
+ * @param {*} urls array contenente tutti gli URL brands.js, tranne gli URL delle auto già scaricate nel cars-specs.json
+ * @param {*} startindex l'indice dal quale si comincia a scorrere gli URL dell'array parametro
+ */
+function ricorsiva(urls, startindex, finalindex) {
+    
+    console.time("scrape execution"); // ti mostra quanto dura l'estrazione di una singola auto
+
+    scrape(urls[startindex])
+    .then((data) => {
+        let jsonData = []; // creo un jsonData che è un array vuoto
+    
+        try {
+    
+            const fileData = fs.readFileSync(path.join("./output", "cars-specs.json"), "utf8"); // se esiste, leggo il cars-specs.json
+            jsonData = JSON.parse(fileData); // convert JavaScript values to and from the JavaScript Object Notation (JSON) format
+    
+        } catch (err) {
+    
+            if (err.code !== "ENOENT") {
+                console.log("Errore nella lettura del file: " + err.message);
+            } else {
+                console.log("> il file non esiste, creo il file...");
+                fs.writeFileSync(path.join("./output", "cars-specs.json"), "[]", { flag: "w" }); // se il file cars-specs.json non esiste, lo creo
+            }
+        }
+    
+        console.log(`\n> aggiungo ${data["Marca"]} ${data["Modello"]} ${data["Inizio produzione"]} in output/cars-specs.json...`);
+    
+        jsonData.push(data);
+        fs.writeFileSync(path.join("./output", "cars-specs.json"), JSON.stringify(jsonData, null, 2), { flag: "w" }); // aggiungo al cars-specs.json il nuovo oggetto
+    
+    })
+    .then(() => {
+
+        // quando finisce l'array di urls finisce anche l'esecuzione periodica di scrape()
+        if(startindex !== finalindex) {
+
+            setTimeout(() => {
+
+                console.timeEnd("scrape execution");
+                ricorsiva(urls, (startindex + 1), finalindex);
+                
+            }, Math.round(Math.random() * 7000) + 8000);
+
+        } else {
+            console.timeEnd("scrape execution");
+            console.log("\n> scraping eseguito con successo!")
+            return;
+        }
+    })
+    .catch((err) => {
+    
+        console.log("Errore: " + err.message);
+    });
+}
+
+module.exports = ricorsiva;
---
+++ src/utils/functions/scrape-function.js
@@ -0,0 +1,84 @@
+const puppeteer = require("puppeteer");
+
+/**
+ * La funzione serve a fare del webscraping mirato a tirare giù i dati tecnici delle automobili nel sito di automoto.it
+ * @param {string} url accetta un url qualsiasi del sito di automoto contenente una scheda tecnica di automobile 
+ * @returns {object} che contiene le key-value con i dati presi dalla tabella della scheda tecnica
+ */
+async function scrape (url) {
+
+    const browser = await puppeteer.launch({headless: "new"});
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    const tableRows = await page.$$(".datagrid > tbody > tr");
+
+    const data = {}; // inizializzo un oggetto "data" vuoto
+    data["url"] = url; // pusho in data l'url da dove provengono i dati
+
+    /**
+     * Definisco quali sono le key dei dati che voglio considerare
+     */
+    const acceptableKeys = [
+        "marca",
+        "modello",
+        "prezzo",
+        "inizio produzione",
+        "fine produzione",
+        "carrozzeria",
+        "porte",
+        "posti",
+        "bagagliaio",
+        "capacità serbatoio",
+        "massa in ordine di marcia",
+        "lunghezza",
+        "larghezza",
+        "altezza",
+        "passo",
+        "motore",
+        "cilindrata",
+        "alimentazione",
+        "potenza max/regime",
+        "coppia massima",
+        "trazione",
+        "cambio",
+        "marce",
+        "velocità max",
+        "accelerazione 0-100km/h",
+        "emissioni co2",
+        "omologazione antinquin.",
+        "urbano",
+        "extraurbano",
+        "misto",
+        "consumo combinato",
+        "tipo ibrido",
+        "potenza (termico + elettrico)",
+        "coppia (termico + elettrico)",
+        "massa rimorchiabile max",
+        "autonomia motore elettrico",
+        "potenza massima",
+        "potenza di picco"
+    ];
+
+    /**
+     * Per ogni tabella della pagina web dell'url fornito mi prendo il testo contenuto nei th, contenitori delle key e il testo dei td, contenitori dei values
+     * Uso la regex e trim() per pulire le stringhe in ingresso
+     * Eseguo un controllo sulle key e, se rientrano nelle accettabili, le pusho in data
+     */
+    for(const tableRow of tableRows) {
+        const th = await page.evaluate(element => element.querySelector("th").textContent, tableRow);
+        const td = await page.evaluate(element => element.querySelector("td").textContent, tableRow);
+
+        const key = th.replace(/\s{2,}/g, ' ').trim();
+        const value = td.replace(/\s{2,}/g, ' ').trim();
+
+        if(acceptableKeys.includes(key.toLowerCase())) {
+            data[key] = value;
+        };
+    }
+
+    await browser.close();
+    return data;
+};
+
+module.exports = scrape;
